e75d1860ddad48bd3e06492f40780dc4798f4f50
TaroMori0321 <87011031+TaroMori0321@users.noreply.github.com>
2023-06-27 23:33:04 +0900

    Update Container.php
    Diコンテナとして最低限動くよう処理を追加。一度生成済みのインスタンスを溜め込み、再利用するようにした。

--- src/Container.php
+++ src/Container.php
@@ -8,11 +8,29 @@ use Psr\Container\ContainerInterface;
 
 class Container implements ContainerInterface
 {
+    /**
+     * 生成処理を格納する
+     * @var array
+     */
+    protected array $definitions;
+
+    /**
+     * 生成された依存性を格納する
+     * @var array
+     */
+    protected array $dependencies;
+
     public function get(string $id): mixed
     {
+        return $this->dependencies[$id] ??= $this->definitions[$id];
     }
 
     public function has(string $id): bool
     {
+        return isset($this->dependencies[$id]) || isset($this->definitions[$id]);
+    }
+
+    public function add(string $id, mixed $definition): void {
+        $this->definitions[$id] = $definition;
     }
 }
