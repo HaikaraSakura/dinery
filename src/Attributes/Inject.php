ce39307999d6603512d98ee992edf1eaf6ae02ff
TaroMori0321 <87011031+TaroMori0321@users.noreply.github.com>
2023-06-28 13:34:13 +0900

    refactor

--- src/Attributes/Inject.php
+++ src/Attributes/Inject.php
@@ -9,11 +9,8 @@ use Attribute;
 #[Attribute]
 class Inject
 {
-    protected string $id;
-
-    public function __construct(string $id)
+    public function __construct(protected string $id)
     {
-        $this->id = $id;
     }
 
     public function getId(): string
